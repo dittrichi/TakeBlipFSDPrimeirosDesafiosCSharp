f76f55117db6b91eaaba362b830a52335c962a84
Dittrichi <marco.dittrichi@gmail.com>
2021-10-14 11:24:24 +0200

    TakeBlipFSDPrimeirosDesafiosCSharp

---
+++ Diferenca.cs
@@ -0,0 +1,15 @@
+using System;
+namespace PrimeirosDesafiosCSharp
+{
+    public class Diferenca
+    {
+        public static void Executar()
+        {
+            int[] variavel = new int[4]; //declare suas variaveis;
+            for (int i = 0; i < 4; i++)
+                variavel[i] = int.Parse(Console.ReadLine());                        
+            Console.WriteLine("DIFERENCA = {0}", (variavel[0] * variavel[1]) - (variavel[2] * variavel[3]));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeirosDesafiosCSharp
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            //PrimeirosDesafiosCSharp.Diferenca.Executar();
+            PrimeirosDesafiosCSharp.Problem.Executar();
+        }
+    }
+}
